d6fd513de0ddc9cb013c86932208174fdcfcb8a2
maniginam <maniginam@gmail.com>
2020-08-21 14:21:18 -0500

    cleaning up

--- project.clj
+++ project.clj
@@ -1,9 +1,5 @@
-(defproject prime-kata-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :main prime-kata.core
+(defproject katas.clj "0.1.0-SNAPSHOT"
+  :description "kata practice"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
   :plugins [[speclj "3.3.2"]]
--- test/prime_kata_clj/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns prime-kata-clj.core-test
-  (:require [clojure.test :refer :all]
-            [prime-kata-clj.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
