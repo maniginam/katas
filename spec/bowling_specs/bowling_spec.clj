0ecb8294a8f2dde6feab9d436b5ca38f4a3d290c
maniginam <maniginam@gmail.com>
2020-08-31 11:47:56 -0500

    bowling attempt 6

--- spec/bowling_specs/bowling_spec.clj
+++ spec/bowling_specs/bowling_spec.clj
@@ -3,27 +3,25 @@
             [bowling.bowling :refer :all]))
 
 (defn roll-rest-of-game [rolls pins]
-  (apply (partial conj rolls) (repeat (- 20 (count rolls)) pins)))
-
+  (apply (partial conj rolls) (repeat (- 20 (count rolls)) 0)))
 
 (describe "bowling kata"
 
   (it "bowls gutter game"
-    (should= 0 (score (repeat 20 0))))
+    (should= 0 (score 0 (repeat 20 0))))
 
   (it "bowls 1 pin down each roll"
-    (should= 20 (score (repeat 20 1))))
+    (should= 20 (score 0 (repeat 20 1))))
 
   (it "bowls 1 spare"
-    (should= 16 (score (roll-rest-of-game [5 5 3] 0))))
+    (should= 16 (score 0 (roll-rest-of-game [5 5 3] 0))))
 
   (it "bowls 1 strike"
-    (should= 16 (score (roll-rest-of-game [10 1 2] 0))))
+    (should= 16 (score 0 (roll-rest-of-game [10 1 2] 0))))
 
   (it "bowls 1 strike then 0"
-    (should= 16 (score (roll-rest-of-game [10 0 3] 0))))
+    (should= 14 (score 0 (roll-rest-of-game [10 0 2] 0))))
 
   (it "bowls 1 spare then 0"
-    (should= 16 (score (roll-rest-of-game [5 5 0 6] 0))))
-
+    (should= 19 (score 0 (roll-rest-of-game [5 5 0 2 3 4] 0))))
   )
--- src/bowling/bowling.clj
+++ src/bowling/bowling.clj
@@ -1,14 +1,13 @@
 (ns bowling.bowling)
 
-(defn is-strike? [[roll1 :as rolls]]
+(defn is-strike? [[roll1 & rolls]]
   (= 10 roll1))
 
-(defn is-spare? [[roll1 roll2 :as rolls]]
+(defn is-spare? [[roll1 roll2 & rolls]]
   (and (not (is-strike? rolls)) (= 10 (+ roll1 roll2))))
 
-(defn score [rolls]
-  (loop [rolls rolls frame 10 score 0]
-    (cond (zero? frame) score
-          (is-strike? rolls) (recur (drop 1 rolls) (dec frame) (apply + score (take 3 rolls)))
-          (is-spare? rolls) (recur (drop 2 rolls) (dec frame) (apply + score (take 3 rolls)))
-          :else (recur (drop 2 rolls) (dec frame) (apply + score (take 2 rolls))))))
+(defn score [game-score rolls]
+    (cond (> 2 (count rolls)) game-score
+          (is-strike? rolls) (score (apply + game-score (take 3 rolls)) (drop 1 rolls))
+          (is-spare? rolls) (score (apply + game-score (take 3 rolls)) (drop 2 rolls))
+          :else (score (apply + game-score (take 2 rolls)) (drop 2 rolls))))
