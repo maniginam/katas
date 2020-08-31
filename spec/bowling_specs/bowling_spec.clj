41229171fbb3a8e63e6b362932b3f91c539419c1
maniginam <maniginam@gmail.com>
2020-08-31 11:59:22 -0500

    modified attempt 6

--- spec/bowling_specs/bowling_spec.clj
+++ spec/bowling_specs/bowling_spec.clj
@@ -8,20 +8,23 @@
 (describe "bowling kata"
 
   (it "bowls gutter game"
-    (should= 0 (score 0 (repeat 20 0))))
+    (should= 0 (score 0 10 (repeat 20 0))))
 
   (it "bowls 1 pin down each roll"
-    (should= 20 (score 0 (repeat 20 1))))
+    (should= 20 (score 0 10 (repeat 20 1))))
 
   (it "bowls 1 spare"
-    (should= 16 (score 0 (roll-rest-of-game [5 5 3] 0))))
+    (should= 16 (score 0 10 (roll-rest-of-game [5 5 3] 0))))
 
   (it "bowls 1 strike"
-    (should= 16 (score 0 (roll-rest-of-game [10 1 2] 0))))
+    (should= 16 (score 0 10 (roll-rest-of-game [10 1 2] 0))))
+
+  (it "bowls perfect game"
+    (should= 300 (score 0 10 (repeat 12 10))))
 
   (it "bowls 1 strike then 0"
-    (should= 14 (score 0 (roll-rest-of-game [10 0 2] 0))))
+    (should= 14 (score 0 10 (roll-rest-of-game [10 0 2] 0))))
 
   (it "bowls 1 spare then 0"
-    (should= 19 (score 0 (roll-rest-of-game [5 5 0 2 3 4] 0))))
+    (should= 19 (score 0 10 (roll-rest-of-game [5 5 0 2 3 4] 0))))
   )
--- src/bowling/bowling.clj
+++ src/bowling/bowling.clj
@@ -6,8 +6,8 @@
 (defn is-spare? [[roll1 roll2 & rolls]]
   (and (not (is-strike? rolls)) (= 10 (+ roll1 roll2))))
 
-(defn score [game-score rolls]
-    (cond (> 2 (count rolls)) game-score
-          (is-strike? rolls) (score (apply + game-score (take 3 rolls)) (drop 1 rolls))
-          (is-spare? rolls) (score (apply + game-score (take 3 rolls)) (drop 2 rolls))
-          :else (score (apply + game-score (take 2 rolls)) (drop 2 rolls))))
+(defn score [current-score frames rolls]
+    (cond (zero? frames) current-score
+          (is-strike? rolls) (score (apply + current-score (take 3 rolls)) (dec frames) (drop 1 rolls))
+          (is-spare? rolls) (score (apply + current-score (take 3 rolls)) (dec frames) (drop 2 rolls))
+          :else (score (apply + current-score (take 2 rolls)) (dec frames) (drop 2 rolls))))
