1369c5d8c9e5da26a7e904907d49fc69cad52561
maniginam <maniginam@gmail.com>
2020-09-11 09:39:15 -0500

    coin changer 2

--- src/coin_changer.clj
+++ src/coin_changer.clj
@@ -1,13 +1,11 @@
 (ns coin-changer)
 
-(defn get-coins [pennies coin-value]
-  (/ (- pennies (mod pennies coin-value)) coin-value))
-
-(defn coin-changer [pennies]
-  (loop [coins {:quarters 0 :dimes 0 :nickels 0 :pennies 0}
+(defn change [pennies]
+  (loop [coins []
          pennies pennies]
-    (cond (< pennies 5) (assoc coins :pennies pennies)
-          (< pennies 10) (recur (assoc coins :nickels (get-coins pennies 5)) (mod pennies 5))
-          (< pennies 25) (recur (assoc coins :dimes (get-coins pennies 10)) (mod pennies 10))
-          :else (recur (assoc coins :quarters (get-coins pennies 25)) (mod pennies 25))
-          )))
+    (cond (zero? pennies) coins
+          (>= (- pennies 25) 0) (recur (conj coins 25) (- pennies 25))
+          (>= (- pennies 10) 0) (recur (conj coins 10) (- pennies 10))
+          (>= (- pennies 5) 0) (recur (conj coins 5) (- pennies 5))
+          (>= (- pennies 1) 0) (recur (conj coins 1) (dec pennies)))
+    ))
