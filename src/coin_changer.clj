b270109071cf36e67cd8bd826557b06ffa3152e0
maniginam <maniginam@gmail.com>
2020-09-12 12:58:16 -0500

    coin changer 5

--- src/coin_changer.clj
+++ src/coin_changer.clj
@@ -1,7 +1,9 @@
 (ns coin-changer)
 
-(defn coin-changer [pennies]
-  (let [values [25 10 5 1]
-        cents-per-coins (reductions #(rem %1 %2) pennies values)
-        coins (map #(int (/ %1 %2)) cents-per-coins values)]
-    (mapcat #(repeat %1 %2) coins values)))
+(defn coin-changer [cents]
+  (if (zero? cents)
+    []
+    (let [values [25 10 5 1]
+          cents-per-coin (reductions #(rem %1 %2) cents values)
+          coins (map #(int (/ %1 %2)) cents-per-coin values)]
+      (mapcat #(repeat %1 %2) coins values))))
