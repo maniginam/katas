6608490dbcbfc47ab45ca47c13bef44910d5ad63
maniginam <maniginam@gmail.com>
2020-11-09 09:54:40 -0600

    new iteration of game of life

--- game-of-life/spec/game_of_life_specs.clj
+++ game-of-life/spec/game_of_life_specs.clj
@@ -4,15 +4,14 @@
 
 (def dead-world #{})
 (def lonely-world #{[0 0]})
-(def world-of-two #{[0 0] [1 0]})
+(def world-of-two #{[0 0] [0 1]})
 (def diagonal-world #{[-1 1] [0 0] [1 -1]})
-(def block-world #{[0 0] [0 1] [1 1] [1 0]})
-(def L-world #{[0 0] [0 1] [1 0]})
+(def block-world #{[0 0] [1 0] [1 1] [0 1]})
+(def L-world #{[0 0] [1 0] [0 1]})
 
 (describe "Game of Life:"
-
   (context "evolves"
-    (it "a dead wolrd"
+    (it "a dead world"
       (should= dead-world (evolve dead-world)))
 
     (it "a lonely world"
@@ -24,19 +23,19 @@
     (it "a diagonal world"
       (should= lonely-world (evolve diagonal-world)))
 
-    (it "a block-world"
+    (it "a block world"
       (should= block-world (evolve block-world)))
 
-    (it "an L-world"
+    (it "an L world"
       (should= block-world (evolve L-world))))
 
   (context "counts living neighbors in"
     (it "a lonely world"
-      (should= 0 (count-neighbors lonely-world [0 0])))
+      (should= 0 (count-live-neighbors lonely-world [0 0])))
 
     (it "a world of two"
-      (should= 1 (count-neighbors world-of-two [0 0])))
+      (should= 1 (count-live-neighbors world-of-two [0 0])))
 
     (it "a diagonal world"
-      (should= 2 (count-neighbors diagonal-world [0 0]))))
+      (should= 2 (count-live-neighbors diagonal-world [0 0]))))
   )
--- game-of-life/src/game_of_life.clj
+++ game-of-life/src/game_of_life.clj
@@ -6,16 +6,16 @@
     [(dec x) y] [(inc x) y]
     [(dec x) (dec y)] [x (dec y)] [(inc x) (dec y)]})
 
-(defn count-neighbors [world cell]
-  (count (filter world (get-neighbors cell))))
+(defn count-live-neighbors [world cell]
+    (count (filter world (get-neighbors cell))))
 
-(defn evolve-cell [world [cell neighbor-counts]]
-  (cond (= 3 neighbor-counts) cell
-        (= 2 neighbor-counts) (when (contains? world cell) cell)))
+(defn evolve-cell [world [cell neighbors]]
+  (cond (= 3 neighbors) cell
+        (= 2 neighbors) (when (contains? world cell) cell)))
 
 (defn evolve [world]
   (let [neighbors (mapcat #(get-neighbors %) world)
         cells-to-evolve (set/union world neighbors)
-        neighbor-counts (zipmap cells-to-evolve (map #(count-neighbors world %) cells-to-evolve))
-        new-world (map #(evolve-cell world %) neighbor-counts)]
+        cells-and-neighbor-counts (zipmap cells-to-evolve (map #(count-live-neighbors world %) cells-to-evolve))
+        new-world (map #(evolve-cell world %) cells-and-neighbor-counts)]
     (set (remove nil? new-world))))
