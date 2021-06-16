06f4191d560ab7e8b924533750f3e3063b4aca9d
maniginam <maniginam@gmail.com>
2021-06-15 20:08:17 -0500

    another little change

--- src/minimax/minimax.clj
+++ src/minimax/minimax.clj
@@ -1,6 +1,7 @@
 (ns minimax.minimax)
 
 (def wins [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]])
+(def pieces {1 "X" -1 "O"})
 
 (defn is-win? [board]
 		(some true? (map #(and (= (nth board (first %)) (nth board (second %)))
@@ -8,9 +9,6 @@
 
 (defn is-game-over? [board]
 		(or (is-win? board) (every? string? board)))
-
-(defn get-piece [player]
-		(if (= 1 player) "X" "O"))
 
 (defn open-boxes [board]
 		(filter integer? board))
@@ -27,13 +25,13 @@
 (defn find-best-score [board player depth]
 		(if (is-game-over? board)
 				(set-score board player depth)
-				(let [boards (map #(assoc board % (get-piece (* player -1))) (open-boxes board))
+				(let [boards (map #(assoc board % (get pieces (* player -1))) (open-boxes board))
 										scores (map #(find-best-score % (* player -1) (inc depth)) boards)]
 						(minimax player scores))))
 
 (defn find-best-box [board player]
 		(let [boxes      (open-boxes board)
-								boards     (map #(assoc board % (get-piece player)) boxes)
+								boards     (map #(assoc board % (get pieces player)) boxes)
 								box-scores (map #(find-best-score % player 0) boards)
 								best-score (minimax (* player -1) box-scores)]
 				(first (best-boxes boxes box-scores best-score))))
