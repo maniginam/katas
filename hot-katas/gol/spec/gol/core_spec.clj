16c11a29be8694a16dd1d594b0c319b895a5ed0e
maniginam <maniginam@gmail.com>
2021-09-24 15:36:12 -0500

    init commit

---
+++ hot-katas/gol/spec/gol/core_spec.clj
@@ -0,0 +1,7 @@
+(ns gol.core-spec
+  (:require [speclj.core :refer :all]
+            [gol.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
---
+++ hot-katas/gol/src/gol/core.clj
@@ -0,0 +1,3 @@
+(ns gol.core)
+
+
